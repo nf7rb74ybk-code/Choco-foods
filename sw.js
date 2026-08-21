5b4593ceb5e011ec594d05faac5808c0a9fe43f5
nf7rb74ybk-code <nf7rb74ybk@privaterelay.appleid.com>
2026-08-21 18:56:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,7 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        self.clients.claim()
-    );
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("push", event => {
@@ -13,9 +11,7 @@ self.addEventListener("push", event => {
     let data = {};
 
     try {
-        data = event.data
-            ? event.data.json()
-            : {};
+        data = event.data ? event.data.json() : {};
     } catch (e) {
         data = {
             title: "🚚 CHOCO SHIP",
@@ -25,92 +21,46 @@ self.addEventListener("push", event => {
         };
     }
 
-    const title =
-        data.title ||
-        "🚚 CHOCO SHIP - ĐƠN MỚI";
-
-    const options = {
-
-        body:
-            data.body ||
-            "🔔 Bạn có đơn hàng mới!",
-
-        icon:
-            data.icon ||
-            "./icon-192.png",
-
-        badge:
-            data.badge ||
-            "./icon-192.png",
-
-        vibrate: [
-            300,
-            100,
-            300
-        ],
-
-        requireInteraction: true,
-
-        data: {
-            url:
-                data.url ||
-                "./shipper.html"
-        }
-    };
-
     event.waitUntil(
         self.registration.showNotification(
-            title,
-            options
+            data.title || "🚚 CHOCO SHIP",
+            {
+                body: data.body || "Bạn có đơn hàng mới!",
+                icon: "./icon-192.png",
+                badge: "./icon-192.png",
+                vibrate: [300,100,300],
+                requireInteraction: true,
+                data: {
+                    url: "./shipper.html"
+                }
+            }
         )
     );
 });
 
+self.addEventListener("notificationclick", event => {
 
-self.addEventListener(
-    "notificationclick",
-    event => {
+    event.notification.close();
 
-        event.notification.close();
+    event.waitUntil(
+        self.clients.matchAll({
+            type: "window",
+            includeUncontrolled: true
+        }).then(list => {
 
-        const url =
-            event.notification.data &&
-            event.notification.data.url
-                ? event.notification.data.url
-                : "./shipper.html";
+            for (const client of list) {
 
-        event.waitUntil(
+                if ("focus" in client) {
+                    client.navigate("./shipper.html");
+                    return client.focus();
+                }
 
-            self.clients
-                .matchAll({
-                    type: "window",
-                    includeUncontrolled: true
-                })
-                .then(clients => {
+            }
 
-                    for (
-                        const client of clients
-                    ) {
+            if (self.clients.openWindow) {
+                return self.clients.openWindow("./shipper.html");
+            }
 
-                        if ("focus" in client) {
-
-                            client.navigate(url);
-
-                            return client.focus();
-                        }
-                    }
-
-                    if (
-                        self.clients.openWindow
-                    ) {
-
-                        return self.clients.openWindow(url);
-
-                    }
-
-                })
-
-        );
-
-    }
-);
+        })
+    );
+});
