12deaf330e39aecc55eb933dacb3f1e900036d15
nf7rb74ybk-code <nf7rb74ybk@privaterelay.appleid.com>
2026-08-22 13:42:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,56 +11,137 @@ self.addEventListener("push", event => {
     let data = {};
 
     try {
-        data = event.data ? event.data.json() : {};
-    } catch (e) {
+        data = event.data
+            ? event.data.json()
+            : {};
+    } catch (error) {
+
         data = {
             title: "🚚 CHOCO SHIP",
             body: event.data
                 ? event.data.text()
-                : "Bạn có đơn hàng mới!"
+                : "🔔 Có đơn hàng mới!"
         };
     }
 
+    const title =
+        data.title ||
+        "🚚 CHOCO SHIP";
+
+    const body =
+        data.body ||
+        "🔔 Có đơn hàng mới!";
+
+    const notificationData =
+        data.data ||
+        {};
+
     event.waitUntil(
+
         self.registration.showNotification(
-            data.title || "🚚 CHOCO SHIP",
+            title,
             {
-                body: data.body || "Bạn có đơn hàng mới!",
-                icon: "./icon-192.png",
-                badge: "./icon-192.png",
-                vibrate: [300,100,300],
-                requireInteraction: true,
+                body: body,
+
+                icon:
+                    "./icon-192.png",
+
+                badge:
+                    "./icon-192.png",
+
+                vibrate:
+                    [300,100,300],
+
+                requireInteraction:
+                    true,
+
+                tag:
+                    notificationData.orderId
+                    ? "order-" +
+                      notificationData.orderId
+                    : "choco-ship-order",
+
+                renotify:
+                    true,
+
                 data: {
-                    url: "./shipper.html"
+                    url:
+                        notificationData.url ||
+                        "https://nf7rb74ybk-code.github.io/Choco-foods/shipper.html",
+
+                    orderId:
+                        notificationData.orderId ||
+                        null
                 }
             }
         )
+
     );
 });
 
-self.addEventListener("notificationclick", event => {
 
-    event.notification.close();
+self.addEventListener(
+    "notificationclick",
+    event => {
 
-    event.waitUntil(
-        self.clients.matchAll({
-            type: "window",
-            includeUncontrolled: true
-        }).then(list => {
+        event.notification.close();
 
-            for (const client of list) {
+        const url =
+            event.notification.data &&
+            event.notification.data.url
+                ? event.notification.data.url
+                : "https://nf7rb74ybk-code.github.io/Choco-foods/shipper.html";
 
-                if ("focus" in client) {
-                    client.navigate("./shipper.html");
-                    return client.focus();
+        event.waitUntil(
+
+            self.clients.matchAll({
+                type: "window",
+                includeUncontrolled: true
+            }).then(clients => {
+
+                for (
+                    const client
+                    of clients
+                ) {
+
+                    if (
+                        "focus"
+                        in client
+                    ) {
+
+                        return client
+                            .focus()
+                            .then(() => {
+
+                                if (
+                                    "navigate"
+                                    in client
+                                ) {
+
+                                    return client.navigate(
+                                        url
+                                    );
+
+                                }
+
+                            });
+
+                    }
+
                 }
 
-            }
+                if (
+                    self.clients.openWindow
+                ) {
 
-            if (self.clients.openWindow) {
-                return self.clients.openWindow("./shipper.html");
-            }
+                    return self.clients.openWindow(
+                        url
+                    );
 
-        })
-    );
-});
+                }
+
+            })
+
+        );
+    }
+);
