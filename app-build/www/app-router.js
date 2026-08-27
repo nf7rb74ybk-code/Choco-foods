62371bffaa6cb5ee19e3fd12f698722275362c32
nf7rb74ybk-code <nf7rb74ybk@privaterelay.appleid.com>
2026-08-28 01:14:35 +0700

    Fix app router login navigation

--- app-build/www/app-router.js
+++ app-build/www/app-router.js
@@ -1,5 +1,5 @@
 (() => {
   const routes = { customer: 'customer.html', shipper: 'shipper.html', admin: 'admin.html' };
   window.openRole = role => { if (routes[role]) location.href = routes[role]; };
-  window.openLogin = () => alert('Màn hình đăng nhập Supabase sẽ được nối ở bước kế tiếp.');
+  window.openLogin = () => { location.href = 'login.html'; };
 })();
